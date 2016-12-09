c63f1a46ea26200fbe02791886e4f0dbad836502
Edward Larsen <E2Larsen@gmail.com>
2016-12-08 21:05:05 -0800

    Added Pricing Page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   	get 'signup' => 'devise/registrations#new'	
   end
   root 'pages#home'
+  get 'pricing' => 'pages#pricing'
   get 'about' => 'pages#about'
   get 'contactus' => 'pages#contactus'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
